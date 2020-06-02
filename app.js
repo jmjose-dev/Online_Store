3d16bf979ba7a45fa4faa75487e5fdb5f5553efe
Jan Michael Jose <janmichael.jose@gmail.com>
2020-06-02 22:46:48 +0800

    connected the database

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var ex = require("express");
 var ej = require("ejs");
 var app = ex();
 var port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = 'logincreds'
+let db
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+
+  // Storing a reference to the database so you can use it later
+  db = client.db(dbName)
+  console.log(`Connected MongoDB: ${url}`)
+  console.log(`Database: ${dbName}`)
+})
 app.use(ex.static("public"));
 app.get("/", function (req, res) {
   res.render("index.ejs");
