3f6477f7c8b5ac9b9abaf87fb6e96015165b5abd
Jan Michael Jose <janmichael.jose@gmail.com>
2020-06-09 16:45:08 +0800

    fixed login procedure

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var mongoose = require("mongoose");
 var app = ex();
 var bP = require("body-parser"); 
 var port = process.env.PORT || 3000;
-var isLoggedIn = false;
+var isLoggedIn = 0;
+var firstTime = 1;
+var fn, fullname,datelog,timelog;
+var d = new Date();
 const request = require('request');
 //define database location
 const url = 'mongodb://127.0.0.1:27017/logincreds';
@@ -15,28 +18,39 @@ mongoose.connect(url, {useNewUrlParser: true});
 //define schema of Login Collection
 var loginSchema = new mongoose.Schema({
 	uname: String,
-	pword: String
+  pword: String,
+  fulln: String
+});
+var loginlogsSchema = new mongoose.Schema({
+  LogFullN: String,
+  LogDate: String,
+  LogTime: String,
+  LogCount: String
 });
 //connect Login schema to loginProcess object
 var loginProcess = mongoose.model("logins",loginSchema);
+//Connect Loginlogs Schema to loginlogs object
+var loginLogs = mongoose.model("loginlogs",loginlogsSchema);
+
 app.use(bP.urlencoded({extended: true}));
 app.use(ex.static("public"));
 
 app.get("/", function (req, res) {
-  if (!isLoggedIn)
+  if (isLoggedIn==0)
     {
       res.redirect("/login");
     }
   else
     {
-      res.render("index.ejs");
+      res.render("index.ejs",{fullname:fn,dl:datelog,tl:timelog});
     }
 });
 app.get("/login", function(req,res){
-  res.render("login.ejs");
+  res.render("login.ejs",{isLogged:isLoggedIn,ft:firstTime});
 });
 app.post("/loginprocess", function(req,res){
-  var un,pw;
+  var un,pw,numlog=0;
+  var newLog;
   un=req.body.txt_uname;
   pw=req.body.txt_pword;
   loginProcess.find({uname:un, pword:pw}, function(err, data){
@@ -46,12 +60,30 @@ app.post("/loginprocess", function(req,res){
   }
   if(data.length == 0) {
     console.log("No record found");
-    isLoggedIn = false;
+    isLoggedIn = 0;
+    firstTime = 0;
     res.redirect("/");
     return
   }
   else{
-    isLoggedIn = true;
+    isLoggedIn = 1;
+    fn = data[0].fulln;
+    numlog = loginLogs.countDocuments({LogFullN:fn});
+    numlog++;
+    datelog = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+    timelog = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    newlog = new loginLogs({LogFullN:fn,LogDate:datelog,LogTime:timelog,LogCount:numlog});
+    newlog.save(function (err,book) {
+      
+        if (err)
+          {
+            return console.error(err);
+          }
+        else
+          {
+            console.log("Login logged!");
+          }
+      });
     res.redirect("/");
     }
  });
